429fc494d9242b88a5917a1795226232dc06ccb4
JumpAsLifeStyle <JumpAsLifeStyle@gmail.com>
2015-04-15 16:28:47 +0300

    Contacts list

--- Jumper/src/com/example/jumper/ChooseGroup.java
+++ Jumper/src/com/example/jumper/ChooseGroup.java
@@ -1,92 +1,81 @@
 package com.example.jumper;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
-import android.database.Cursor;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.jumper.utils.impl.ContactsProviderImpl;
+import com.example.jumper.utils.interfaces.ContactsProvider;
+
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ChooseGroup extends ActionBarActivity {
+public class ChooseGroup extends ListActivity {
 
+	private static ContactsProvider m_contactsProvider;
+	private static String[] m_contactNumbers;
+	private static String[] m_contactNames;
+
+	String[] city= {
+			"Bangalore",
+			"Chennai",
+			"Mumbai",
+			"Pune",
+			"Delhi",
+			"Jabalpur",
+			"Indore",
+			"Ranchi",
+			"Hyderabad",
+			"Ahmedabad",
+			"Kolkata",
+			"Bhopal"
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_choose_group);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new PlaceholderFragment()).commit();
+		setContentView(R.layout.fragment_choose_group);
+
+		/* OUR CODE BEGINS HERE */
+		m_contactsProvider = new ContactsProviderImpl(this);
+
+		listSetUp();
+	}
+
+	private void listSetUp() {
+		// Display mode of the ListView
+		ListView listview= getListView();
+		listview.setChoiceMode(listview.CHOICE_MODE_MULTIPLE);
+
+		// text filtering
+		listview.setTextFilterEnabled(true);
+
+		Map<String, String> contacts = new HashMap<String, String>();;
+		try {
+			contacts = doChooseGroup();
+		} catch (Exception e) {
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
 		}
 
-		/*	OUR CODE BEGINS HERE	*/
+		m_contactNumbers = contacts.keySet().toArray(new String[contacts.keySet().size()]);
+		m_contactNames = contacts.values().toArray(new String[contacts.values().size()]);
 
-		doChooseGroup();
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_checked,m_contactNames));
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.choose_group, menu);
-		return true;
+	public void onListItemClick(ListView parent, View v,int position,long id){
+		CheckedTextView item = (CheckedTextView) v;
+		Toast.makeText(this, m_contactNames[position] + " checked : " +
+				item.isChecked(), Toast.LENGTH_SHORT).show();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_choose_group,
-					container, false);
-			return rootView;
-		}
-	}
-	private void getAllContacts() {
-		Cursor c = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-		String name, number = "";
-		String id;
-		c.moveToFirst();
-		for (int i = 0; i < c.getCount(); i++) {
-			name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-			id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
-
-			if (Integer.parseInt(c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-				Cursor pCur = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[] { id },
-						null);
-				while (pCur.moveToNext()) {
-					number = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-				}
-			}
-			Log.i("name ", name + " ");
-			Log.i("number ", number + " ");
-			c.moveToNext();
-		}
-	}
-
-	public void doChooseGroup() {
-		getAllContacts();
+	public Map<String, String> doChooseGroup() {
+		return m_contactsProvider.provideContacts();
 	};
 }
---
+++ Jumper/src/com/example/jumper/utils/interfaces/ContactsProvider.java
@@ -0,0 +1,7 @@
+package com.example.jumper.utils.interfaces;
+
+import java.util.Map;
+
+public interface ContactsProvider {
+	Map<String, String> provideContacts();
+}
